57da124ecedf986cea82d2c3249fa7e9bd65286c
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-19 23:34:54 +0700

    chore: add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": "off",
+      "no-constant-condition": ["error", { checkLoops: false }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+);
