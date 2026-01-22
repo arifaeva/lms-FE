11b6e44f74e09a47779527bc62e5a0bbe072c18e
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-22 23:34:32 +0700

    feat: add welcome page to home route for testing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,5 +9,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <h1>Halo</h1>
+      <Welcome />
+    </>
+  );
 }
