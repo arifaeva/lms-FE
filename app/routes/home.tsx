687d24e476e95e006862d14b19fe25aef904562f
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-25 22:52:37 +0700

    chore: clean up home page and test tailwind

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,7 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
   return (
     <>
-      <h1>Halo</h1>
-      <Welcome />
+      <h1 className="bg-pinkish">Halo</h1>
     </>
   );
 }
