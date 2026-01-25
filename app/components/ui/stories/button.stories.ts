e4952c91b5982237d72334caaa7974c7cf9700f6
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-25 22:51:55 +0700

    feat(ui): create button stories/documentation

--- app/components/ui/stories/button.stories.ts
+++ app/components/ui/stories/button.stories.ts
@@ -1,21 +1,61 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "../button";
 
-const meta: Meta = {
+const meta = {
   title: "Button",
   component: Button,
+  parameters: {
+    layout: "centered",
+  },
   argTypes: {
-    variant: { type: "string" },
-    size: { type: "string" },
+    variant: {
+      control: { type: "select" },
+      options: ["primary", "secondary", "outline", "text", "danger"],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+    },
+    children: { control: "text" },
   },
   tags: ["autodocs"],
-};
+} satisfies Meta<typeof Button>;
 
 export default meta;
+
 type Story = StoryObj<typeof Button>;
 
 export const ButtonPrimary: Story = {
   args: {
-    children: "Submit!",
+    variant: "primary",
+    children: "Button",
+  },
+};
+
+export const ButtonSecondary: Story = {
+  args: {
+    variant: "secondary",
+    children: "Button",
+  },
+};
+
+export const ButtonOutlined: Story = {
+  args: {
+    variant: "outlined",
+    children: "Button",
+  },
+};
+
+export const ButtonText: Story = {
+  args: {
+    variant: "text",
+    children: "Button",
+  },
+};
+
+export const ButtonDanger: Story = {
+  args: {
+    variant: "danger",
+    children: "Button",
   },
 };
