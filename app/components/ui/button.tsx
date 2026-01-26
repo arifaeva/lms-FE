e2abdb201534ffb16cff363c33ad83316b7515fa
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-26 17:35:23 +0700

    style(button): adjust button component style

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -3,18 +3,18 @@ import { twMerge } from "tailwind-merge";
 import { tv, type VariantProps } from "tailwind-variants";
 
 export const buttonStyle = tv({
-  base: "flex select-none cursor-pointer justify-center rounded-md bg-transparent transition duration-150 focus:outline-pinkish disabled:bg-slate-50 disabled:text-neutral-400",
+  base: "flex select-none cursor-pointer justify-center rounded-md font-medium leading-5 transition duration-150 hover:shadow-sm focus:shadow-none active:shadow-none disabled:bg-neutral-100 disabled:text-neutral-400 shadow-sm active:ring-0 focus:outline-[3px] focus:outline-indigo-600/20 active:outline-0",
   variants: {
     variant: {
       primary:
-        "bg-indigo-600 text-white hover:bg-indigo-600 focus:bg-indigo-600 active:ring-0",
+        "bg-indigo-600 text-white hover:bg-indigo-400 focus:bg-indigo-400 active:bg-indigo-900",
       secondary:
-        "bg-indigo-50 text-indigo-600 hover:text-indigo-400 focus:text-indigo-400 active:text-indigo-900 active:ring-0",
+        "bg-indigo-50 text-indigo-600 hover:text-indigo-400 focus:text-indigo-400 active:text-indigo-900",
       outlined:
-        "bg-white text-neutral-900 ring-1 ring-border hover:bg-zinc-50 hover:ring-border focus:bg-zinc-50 active:ring-0",
-      text: "bg-transparent text-indigo-600 hover:bg-indigo-400 focus:bg-indigo-400 active:text-indigo-900 active:ring-0",
+        "bg-white text-neutral-900 border border-neutral-200 hover:bg-zinc-50 focus:bg-zinc-50 active:ring-0 active:bg-neutral-200",
+      text: "bg-transparent text-indigo-600 hover:text-indigo-400 focus:text-indigo-400 active:text-indigo-900 disabled:bg-transparent disabled:outline-0",
       danger:
-        "bg-rose-600 text-white hover:bg-rose-400 active:bg-rose-900 active:ring-0",
+        "bg-rose-600 text-white hover:bg-rose-400 focus:bg-rose-400 active:bg-rose-900 ",
     },
     size: {
       sm: "px-4 py-1 text-xs",
