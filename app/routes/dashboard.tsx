96e84a089e80962c0d19b92a3b9afd32fd1624b4
arifaeva <arifa.eva.ixa@gmail.com>
2026-02-03 23:34:25 +0700

    feat: create dashboard page

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="font-medium text-3xl tracking-tight">
+        Ini halaman dashboard
+      </h1>
+    </main>
+  );
+}
