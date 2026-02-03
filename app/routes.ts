e59121eef6c4cd2f3ceb10ae09cf5b6e899c64b8
arifaeva <arifa.eva.ixa@gmail.com>
2026-02-03 23:36:09 +0700

    feat(routes): register login page, register page, and dashboard page

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,20 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  layout,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  // Home page → "/"
+  index("routes/home.tsx"),
+
+  // Auth layout → "/auth/*"
+  layout("routes/auth.tsx", [
+    // Login page → "/auth/login"
+    route("auth/login", "routes/auth.login._index.tsx"),
+    route("auth/register", "routes/auth.register._index.tsx"),
+  ]),
+
+  route("dashboard", "routes/dashboard.tsx"),
+] satisfies RouteConfig;
