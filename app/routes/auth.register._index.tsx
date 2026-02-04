bc0f3ec06c4b0fd37f3bfdad26dea054a413f634
arifaeva <arifa.eva.ixa@gmail.com>
2026-02-04 23:46:15 +0700

    style: adjust styling in register page

--- app/routes/auth.register._index.tsx
+++ app/routes/auth.register._index.tsx
@@ -1,8 +1,9 @@
 import type { ActionFunctionArgs } from "react-router";
-import { redirect, useActionData, Form } from "react-router";
+import { redirect, useActionData, Form, Link } from "react-router";
 import { json } from "@remix-run/node";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
+import { API_URL } from "~/config/api.url";
 
 type ActionData = {
   error?: {
@@ -17,7 +18,7 @@ export async function action({ request }: ActionFunctionArgs) {
   const email = formData.get("email");
   const password = formData.get("password");
 
-  const res = await fetch("http://localhost:3000/api/v1/auth/register", {
+  const res = await fetch(`${API_URL}/api/v1/auth/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, email, password }),
@@ -37,25 +38,41 @@ export default function RegisterPage() {
 
   return (
     <main className="flex justify-center items-center">
-      <Form method="post" className="space-y-4 w-xs">
+      <Form method="post" className="space-y-4 w-sm">
         {actionData?.error && (
           <p className="text-sm text-red-600">
             {actionData.error.message ?? "Login failed"}
           </p>
         )}
 
-        <Input name="name" type="name" placeholder="Name" required />
-        <Input name="email" type="email" placeholder="Email" required />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-        />
+        <div className="space-y-1">
+          <h1>Create an account</h1>
+          <p className="leading-5">
+            Create an account to start using the platform
+          </p>
+        </div>
 
-        <Button type="submit" className="w-full">
-          Register
-        </Button>
+        <div className="space-y-3">
+          <Input name="name" type="name" placeholder="Name" required />
+          <Input name="email" type="email" placeholder="Email" required />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+
+          <Button type="submit" className="w-full">
+            Register
+          </Button>
+        </div>
+
+        <p>
+          Already have an account?{" "}
+          <Link to="/auth/login" className="text-indigo-600">
+            Login here.
+          </Link>
+        </p>
       </Form>
     </main>
   );
