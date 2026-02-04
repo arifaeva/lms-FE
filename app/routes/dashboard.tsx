7fc0f04fd916b9116c7d69016897fedac4583b8c
arifaeva <arifa.eva.ixa@gmail.com>
2026-02-04 23:47:10 +0700

    fix: fix data fetching in dashboard page

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,8 +1,25 @@
-export default function Home() {
+import type { LoaderFunctionArgs } from "react-router";
+import { useLoaderData } from "react-router";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const response = await fetch("http://localhost:3000/api/v1/users/me", {
+    headers: request.headers,
+  });
+
+  if (!response.ok) {
+    throw new Response("Unauthorized", { status: 401 });
+  }
+
+  return await response.json();
+}
+
+export default function Dashboard() {
+  const user = useLoaderData<typeof loader>();
+
   return (
     <main className="flex h-screen items-center justify-center">
       <h1 className="font-medium text-3xl tracking-tight">
-        Ini halaman dashboard
+        Hello, {user.name}!
       </h1>
     </main>
   );
