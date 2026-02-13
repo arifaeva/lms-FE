416b9f790449f39a3b1d448bd9df2881d129e33c
arifaeva <arifa.eva.ixa@gmail.com>
2026-02-13 23:42:59 +0700

    style: adjust menu style

--- app/components/ui/menu.tsx
+++ app/components/ui/menu.tsx
@@ -8,7 +8,7 @@ export const menuStyle = tv({
   base: "*:-ml-[1px] group relative grid cursor-pointer select-none grid-cols-[40px_1fr] items-center overflow-hidden text-nowrap rounded-lg border border-transparent p-2.5 text-slate-600 ring-1 ring-transparent transition duration-300 hover:bg-indigo-50 disabled:bg-transparent disabled:text-neutral-500",
   variants: {
     isActive: {
-      true: "border-[0.3px] border-neutral bg-[linear-gradient(180deg,_#4F46E5_0%,_#312E81_138.64%)] text-white shadow-sm outline-[0.3px] outline-indigo-600 ring-1 ring-indigo-600 transition duration-300 hover:bg-[linear-gradient(180deg,_#4338CA_0%,_#211F56_138.64%)] hover:text-white active:bg-[linear-gradient(180deg,_#312E81_0%,_#141335_138.64%)]",
+      true: "border-[0.3px] border-white bg-[linear-gradient(180deg,_#4F46E5_0%,_#312E81_138.64%)] text-white shadow-sm outline-[0.3px] outline-indigo-600 ring-1 ring-indigo-600 transition duration-300 hover:bg-[linear-gradient(180deg,_#4338CA_0%,_#211F56_138.64%)] hover:text-white active:bg-[linear-gradient(180deg,_#312E81_0%,_#141335_138.64%)]",
     },
   },
 });
