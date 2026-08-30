eb88e0696b4edd03ff1ff3da550d3b8f0ba73e02
arifaeva <arifa.eva.ixa@gmail.com>
2026-08-30 20:23:15 +0700

    feat(home): redirect to dashboard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,17 @@
+// export default function Home() {
+//   return (
+//     <main className="flex h-screen items-center justify-center">
+//       <h1 className="font-medium text-3xl tracking-tight">
+//         Woo, nothing here!
+//       </h1>
+//     </main>
+//   );
+// }
+
+import { redirect } from "react-router";
+
+export const loader = () => redirect("/dashboard");
+
 export default function Home() {
-  return (
-    <main className="flex h-screen items-center justify-center">
-      <h1 className="font-medium text-3xl tracking-tight">
-        Woo, nothing here!
-      </h1>
-    </main>
-  );
+  return null;
 }
